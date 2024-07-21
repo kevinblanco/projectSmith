cfcf4d2c5063e9f00e3132d345f7929c87ba1b60
Ron Northcutt <ron@appsmith.com>
2024-07-21 14:22:36 +0000

    updating code

--- pages/Projects/jsobjects/controller/controller.js
+++ pages/Projects/jsobjects/controller/controller.js
@@ -1,28 +1,34 @@
 export default {
 	async onLoad() {
-		await SelectProjectsByUser.run()
-		state.projects = SelectProjectsByUser.data
+		await SelectProjectsByUser.run();
+		state.projects = SelectProjectsByUser.data;
+		if (_.isEmpty(state.current)) {
+			state.currentTab = "list";
+		}
 		this.saveState()
 	},
 	setCurrent(project) {
-		state.current = project
-		state.currentTab = "project"
-		this.saveState()
+		state.current = project;
+		state.currentTab = "project";
+		this.saveState();
 	},
 	isOwner() {
 		// Check if the current email is the same as the owner email
-		
+		return state.current.owner == appsmith.user.email;
 	},
 	saveState() {
-		storeValue("projectsmith", state)
+		storeValue(config.appid, state);
 	},
 	loadState() {
-		let loadedState = appsmith.store.projectsmith || false
+		let loadedState = appsmith.store[config.appid] || false;
 		if (loadedState) {
-			state = _.cloneDeep(loadedState)
+			state = _.cloneDeep(loadedState);
 		}
 	},
 	deleteState() {
-		removeValue("projectsmith")
+		removeValue(config.appid);
+	},
+	test() {
+		console.log(_.isEmpty(state.current));
 	}
 }
