cbdb60341ae5a3988b05f9c45df7aa0f05f6ff90
Kevin Blanco <kevin@appsmith.com>
2024-07-21 15:42:34 +0000

    logic changex

--- pages/Stakeholder Form/jsobjects/controller/controller.js
+++ pages/Stakeholder Form/jsobjects/controller/controller.js
@@ -1,14 +1,16 @@
 export default {
 	async onLoad() {
-		
 		await SelectProjectsByUser.run()
 		state.projects = SelectProjectsByUser.data
+		console.log("STATE PROJECTS:", state.projects)
 
 		const projectId = appsmith.URL.queryParams.project
+		console.log("PROJECT ID:", projectId)
 
-		if (projectId) {
+		if (projectId !== undefined && projectId !== null) {
 			// Try to find the project with the given ID in the SelectUsersByUser
 			const project = state.projects.find(proj => proj.id == projectId)
+			console.log("FOUND PROJECT:", project)
 
 			if (project) {
 				this.setCurrent(project)
@@ -17,8 +19,10 @@ export default {
 				this.redirectToProjectsPage()
 			}
 		} else {
+			console.log("LOAD STATE BECAUSE THERE IS NO PARAMETER")
 			// Load state from local storage
 			this.loadState()
+			console.log("LOADED STATE CURRENT:", state.current)
 
 			if (!state.current) {
 				console.error("No project found in URL query params or local storage")
@@ -26,6 +30,7 @@ export default {
 			}
 		}
 		this.saveState()
+		console.log("CURRENT PROJECT STATE", state.current)
 	},
 
 	setCurrent(project) {
@@ -35,6 +40,7 @@ export default {
 	},
 
 	isOwner() {
+		// Check if the current email is the same as the owner email
 		return appsmith.user.email === state.current.owner
 	},
 
@@ -54,6 +60,7 @@ export default {
 	},
 
 	redirectToProjectsPage() {
-		navigateTo('Projects') 
+		// Redirect user to the projects page or show an error message
+		navigateTo('Projects')  // Adjust the navigation based on your app's configuration
 	}
 }
